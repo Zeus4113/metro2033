a2d82afb35292c3d311d2acad4b038270acfc8fe
Kai Stevens <88673236+Zeus4113@users.noreply.github.com>
2026-07-07 22:34:58 +0100

    Fixed item model

--- schema/items/components/sh_reciever.lua
+++ schema/items/components/sh_reciever.lua
@@ -1,6 +1,6 @@
 ﻿ITEM.name = "Receiver"
 ITEM.description = "A precision-machined firearm receiver required for crafting advanced weapons."
-ITEM.model = "models/illusion/eftcontainers/pressuregauge.mdl"
+ITEM.model = "models/spec45as/stalker/upgrades/gp25.mdl"
 
 ITEM.width = 1
 ITEM.height = 1
